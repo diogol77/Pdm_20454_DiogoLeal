334679e939af61bfc15d02cf7310d638649c0bc4
a20454 <a20454@alunos.ipca.pt>
2024-11-30 19:51:56 +0000

    alteraçoes

--- app/src/main/java/com/example/api_pdm/article/presentation/design/ArticleAction.kt
+++
@@ -1,8 +0,0 @@
-package com.example.api_pdm.article.presentation.design
-
-// Sealed interface para definir as ações possíveis relacionadas ao artigo
-sealed interface ArticleAction {
-
-    // Ação que carrega um artigo com base no seu ID
-    data class LoadArticle(val articleId: String) : ArticleAction
-}
--- app/src/main/java/com/example/api_pdm/news/presentation/design/NewsAction.kt
+++
@@ -1,6 +0,0 @@
-package com.example.api_pdm.news.presentation.design
-
-
-sealed interface NewsAction {
-    data object Paginate: NewsAction
-}
